3804080924a16e37582077e23bb87c1173c26d3b
julia <julia18g11@gmail.com>
2023-12-13 14:39:08 +0300

    fix 3 section

--- 03-section/tasks/02-task.ts
+++ 03-section/tasks/02-task.ts
@@ -7,7 +7,7 @@
 // информации о юзере было указано
 // "Нет данных"
 
-function formattedUserData() {
+function formattedUserData1(firstName: string, lastName: string, age: string = 'Нет данных', hometown: string = 'Нет данных') {
     return `
       Имя: ${firstName};
       Фамилия: ${lastName};
@@ -19,5 +19,13 @@ function formattedUserData() {
   // 2. Ниже перепишите функцию formattedUserData2 таким
   // образом чтобы данные выводились в консоль
   
-  function formattedUserData2() {}
+  function formattedUserData2(firstName: string, lastName: string, age: string = 'Нет данных', hometown: string = 'Нет данных') {
+    console.log( `
+      Имя: ${firstName};
+      Фамилия: ${lastName};
+      Возраст: ${age};
+      Родной город: ${hometown};
+    `)
+  }
+  
   
